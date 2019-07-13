12d21c9f39a5b45b1e14088afa11ed27aa76d230
zackdk <zackvvzackvv@gmail.com>
2019-07-13 13:03:08 +0800

    基本功能完成

--- api/api.js
+++ api/api.js
@@ -2,15 +2,33 @@
 let Bmob = require("./Bmob-1.6.5.min.js");
 const Table_garbage = "garbage";
 
+let ALL_DATA = null;
+
 
 const getAllData = function () {
   const query = Bmob.Query(Table_garbage);
-  return query.find();
+
+  if(ALL_DATA)return Promise.resolve(ALL_DATA);
+  
+  return query.find().then(res=>{
+    ALL_DATA = res;
+    return ALL_DATA
+  })
+}
+
+const getDataByWord = function (word) {
+  if(!ALL_DATA){
+    return getAllData().then(res=>{
+      return res.filter(da => da.name.includes(word))
+    })
+  }else{
+    return Promise.resolve(ALL_DATA.filter(da => da.name.includes(word))) 
+  }
 }
 
 
 
-
 export default {
-  getAllData
+  getAllData,
+  getDataByWord
 }
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,19 +1,25 @@
-// pages/search/search.js
+import constant from "../../utils/constant.js";
+import api from "../../api/api.js";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
     value: "",
-    result: [{ name: "sds", type: 1 }, { name: "dfdf", type: 2 }]
+    loading : "",
+    result: [],
+    constant : null
   },
   onLoad: function (options) {
-
+    this.setData({constant})
   },
   onSearch: function(e){
-    console.log(this.data)
+    console.log(this.data.value)
+    this.setData({loading:true})
+    let that = this;
+    api.getDataByWord(this.data.value)
+    .then(res=>{
+      console.log(res)
+      that.setData({loading:false,result:res})
+    })
   },
   onChange(e) {
     this.setData({
