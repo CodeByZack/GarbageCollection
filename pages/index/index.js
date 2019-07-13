71e706b3a8998285e6e8befdf53bcbbad83429a3
zackdk <zackvvzackvv@gmail.com>
2019-07-13 22:24:26 +0800

    111

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,10 @@ import api from "../../api/api.js";
 
 Page({
   data: {
-    active : 0,
+    active : 1,
     value: "",
     loading: true,
-    list: ["香d蕉皮", "香蕉d皮", "香蕉ss皮", "香蕉皮ss", "香蕉皮dd", "香蕉dd皮", "香蕉a皮", "香蕉w皮", "香蕉e皮", "香蕉d皮",]
+    questions:[{},{},{},{},{}]
   },
   onLoad: function () {
     this.fetchData();
@@ -22,7 +22,6 @@ Page({
     let that = this;
     api.getAllData().then(
       res => {
-        console.log(res)
         let food = res.filter(d => d.type == 0)
         let recycle = res.filter(d => d.type == 1)
         let bad = res.filter(d => d.type == 2)
@@ -47,5 +46,12 @@ Page({
     wx.navigateTo({
       url: `/pages/detail/detail?type=${data.type}&keyword=${data.keyword}`
     })
+  },
+  addNewQuestion: function(){
+    this.setData({show:true})
+    console.log("---------------")
+  },
+  onClose: function(){
+    this.setData({show:false})
   }
 })
